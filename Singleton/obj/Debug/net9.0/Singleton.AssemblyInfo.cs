d84b996fa96fbdddb28000b857285ee31b945423
Einav Haimovich <einav@haimol.net>
2026-02-21 20:07:34 +0200

    update

--- Singleton/obj/Debug/net9.0/Singleton.AssemblyInfo.cs
+++ Singleton/obj/Debug/net9.0/Singleton.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa7e8f718f0b2fb69c2d32c16b51457c5bf9472")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90bbb9905826ae87754e29042203182444083b23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
